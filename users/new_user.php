e453ee14943437277fb247eab9e67e5f348b3335
redelement <michael.t.bryan@gmail.com>
2014-03-17 18:55:24 -0400

    Page for new user creation, initial commit

---
+++ users/new_user.php
@@ -0,0 +1,77 @@
+<?php
+require_once('class.usercontrol.php');
+
+$auth = new UserControl;
+
+$auth->hasAccess(75);
+
+if($_POST) {
+	$auth->create_user($_POST['password'], $_POST['passwordConfirm'], $_POST['firstName'], $_POST['lastName'], $_POST['level']);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>New User</title>
+</head>
+<body>
+	<?php if($auth->getSuccess() !== false) : ?>
+	<div class="success">
+		<?php
+		foreach($auth->getSuccess() as $success) {
+			echo $success .'<br />';
+		}
+		?>
+	</div>
+	<?php endif; ?>
+
+	<?php if($auth->getErrors() !== false) : ?>
+	<div class="errors">
+		<?php
+		foreach($auth->getErrors() as $error) {
+			echo $error .'<br />';
+		}
+		?>
+	</div>
+	<?php endif; ?>
+
+	<h1>New User</h1>
+	<form action="" method="post">
+
+		<p>
+			<label for="firstName">First Name:</label><br />
+			<input type="text" name="firstName" />
+		</p>
+
+		<p>
+			<label for="lastName">Last Name:</label><br />
+			<input type="text" name="lastName" />
+		</p>
+
+		<p>
+			<label for="password">Password:</label><br />
+			<input type="password" name="password" />
+		</p>
+
+		<p>
+			<label for="passwordConfirm">Confirm Password:</label><br />
+			<input type="password" name="passwordConfirm" />
+		</p>
+
+		<p>
+			<label for="level">User Level:</label>
+			<br />
+			<select name="level" id="">
+				<option value="1">Read Only</option>
+				<option value="10">Mid-Level</option>
+				<option value="50">Department Head</option>
+				<option value="75">Manager</option>
+				<option value="100" disabled="disabled">SU</option>
+			</select>
+		</p>
+
+		<input type="submit" name="addUser" value="Add User" />
+	</form>
+</body>
+</html>
