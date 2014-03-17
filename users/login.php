3928ea034007dc1fa564b44bace32f87fceec1ff
redelement <michael.t.bryan@gmail.com>
2014-03-17 18:54:50 -0400

    Login Page initial commit

---
+++ users/login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Login</title>
+</head>
+<body>
+	<h1>Login</h1>
+	<form action="" method="post">
+		<p>
+			<label for="username">Username:</label>
+			<br />
+			<input type="text" name="username" />
+		</p>
+		<p>
+			<label for="password">Password:</label>
+			<br />
+			<input type="password" name="password" />
+		</p>
+		<input type="submit" name="login" value="Login" />
+	</form>
+</body>
+</html>
