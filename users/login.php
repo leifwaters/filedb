4f9109988cdd24a296af83c4900e024b811df9c1
redelement <michael.t.bryan@gmail.com>
2014-03-17 23:32:57 -0400

    Updated the front end some and added validation with error handling

--- users/login.php
+++ users/login.php
@@ -1,3 +1,12 @@
+<?php
+require_once('class.usercontrol.php');
+
+$auth = new UserControl(0, 'login');
+
+if($_POST) {
+	$auth->login($_POST['username'], $_POST['password'], 'admin.php');
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,6 +15,17 @@
 </head>
 <body>
 	<h1>Login</h1>
+
+	<?php if($auth->getErrors() !== false) : ?>
+	<div class="errors">
+		<?php
+		foreach($auth->getErrors() as $error) {
+			echo $error .'<br />';
+		}
+		?>
+	</div>
+	<?php endif; ?>
+
 	<form action="" method="post">
 		<p>
 			<label for="username">Username:</label>
