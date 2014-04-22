7e1319cfb3d872ac6e8eecd4d11f1cde27c8e956
redelement <michael.t.bryan@gmail.com>
2014-04-22 15:56:01 -0400

    added as a default page to go to

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php
+require_once('users/class.usercontrol.php');
+
+$auth = new UserControl(50);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Admin Page</title>
+</head>
+<body>
+	<h1>Admin Page</h1>
+
+</body>
+</html>
