2f1f24e192595aaf5677981719579e9a638ee273
redelement <michael.t.bryan@gmail.com>
2014-05-02 13:48:01 -0400

    Created this page to reset the password on expiration

--- users/pass_reset.php
+++ users/pass_reset.php
@@ -1,7 +1,14 @@
 <?php
 require_once('class.usercontrol.php');
 
-$auth = new UserControl(0);
+$auth = new UserControl(0, 'pass_reset');
+
+if(@$_POST['reset_pass']) {
+
+	$user = $_GET['user'];
+
+	$auth->resetPassword($user, $_POST['new_pass'], $_POST['confirm_pass']);
+}
 
 ?>
 <!doctype html>
@@ -14,7 +21,27 @@ $auth = new UserControl(0);
 	<h1>Your password has expired</h1>
 	<p>In order to comply with HIPPA regulations your password must be reset every 30 days. Do not use the same password.</p>
 
-	<form action="">
+	<?php if($auth->getSuccess() !== false) : ?>
+	<div class="success">
+		<?php
+		foreach($auth->getSuccess() as $success) {
+			echo $success .'<br />';
+		}
+		?>
+	</div>
+	<?php endif; ?>
+
+	<?php if($auth->getErrors() !== false) : ?>
+	<div class="errors">
+		<?php
+		foreach($auth->getErrors() as $error) {
+			echo $error .'<br />';
+		}
+		?>
+	</div>
+	<?php endif; ?>
+
+	<form action="" method="post">
 		<p>
 			<label for="new_pass">New Password</label><br />
 			<input type="password" name="new_pass" />
